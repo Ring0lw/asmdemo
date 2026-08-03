3dd04e00eb6b9584d67c3d23e0b18a92e43f240e
0x4CA <280034184+0x4CA@users.noreply.github.com>
2026-08-03 18:53:25 +0300

    remove tools

--- tools/view.py
+++
@@ -1,7 +0,0 @@
-import sys
-from PIL import Image
-for t in sys.argv[1:]:
-    im = Image.open("s%s.bmp" % t)
-    im = im.resize((im.width*5, im.height*5), Image.NEAREST)
-    im.save("big%s.png" % t)
-    print(t, im.size)
